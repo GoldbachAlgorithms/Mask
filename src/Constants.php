5219c569155e27d1115cce8f533194027674d92c
Goldbach07 <gustavoalves.a7@gmail.com>
2021-10-30 18:27:06 -0300

    Feat: Create methods to default formats.

--- src/Constants.php
+++ src/Constants.php
@@ -4,13 +4,66 @@ namespace GoldbachAlgorithms\Mask;
 
 class Constants
 {
+    /* Supported types */
+    const CEP = "CEP";
+    const CPF = "CPF";
+    const CNPJ = "CNPJ";
+    const DATE = "DATE";
+    const RG = "RG";
+    const PHONE = "PHONE";
+    const CELL_PHONE = "CELL_PHONE";
+    const CUSTOM = "CUSTOM";
+    const CREDIT_CARD = "CREDIT_CARD";
+    
     /**
-     * 🇧🇷 Tamanho exato de cada tipo de informação
-     * 🇺🇸 Exact size of each type of information
-     * 🇮🇹 Dimensione esatta di ogni tipo di informazione
+     * Masks of any supported type
+     *
+     * @const LENGHT
+     */
+    const MASK = [
+        "CEP" => "#####-###",
+        "CPF" => "###.###.###-##",
+        "CNPJ" => "##.###.###/####-##",
+        "DATE" => "##/##/####",
+        "RG" => "##.###.###-#",
+        "PHONE" => "(##) ####-####",
+        "CELL_PHONE" => "(##) #####-####",
+        "CREDIT_CARD" => "####.****.****.####"
+    ];
+
+    /**
+     * Define auto complete type (with zero to the left)
+     *
+     * @const LENGHT
+     */
+    const AUTO_COMPLETE = [
+        "CEP",
+        "CPF",
+        "CNPJ",
+        "RG"
+    ];
+
+    /**
+     * Exact size of each type of information
+     *
+     * @const LENGHT
      */
     const LENGTH = [
         'CEP' => 8,
-        'CPF' => 11
+        'CPF' => 11,
+        'CNPJ' => 14,
+        'RG' => 9,
+        'PHONE' => 10
     ];
+
+    /**
+     * Define mask class errors
+     *
+     * @const ERROR
+     */
+    const ERROR = [
+        'NULL' => 'The value entered is null.',
+        'INVALID' => 'Not supported type entered.'
+    ];
+
 }
--- src/Mask.php
+++ src/Mask.php
@@ -4,43 +4,182 @@ namespace GoldbachAlgorithms\Mask;
 
 use GoldbachAlgorithms\Mask\Constants;
 
+/**
+ * Class to mask predefined or custom values
+ */
 class Mask
 {
-    const FORMAT = [
-        "CEP" => "CEP",
-        "CPF" => "CPF",
-        "CNPJ" => "CNPJ"
-    ];
+    /**
+     * Add a phone mask
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function phone(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::PHONE,
+                        $value
+                    );
+    }
 
-    const CEP = "CEP";
-    const CPF = "CPF";
-    const CNPJ = "CNPJ";
-    
-    const CODE = [
-        "CEP" => "#####-###",
-        "CPF" => "###.###.###-##",
-        "CNPJ" => "##.###.###/####-##"
-    ];
-    
-    public function transform(
-        string $mask,
-        string $value
-    ) {
-        $value = $this->clear($value);
-        $value = $this->rewrite($mask, $value);
-        $value = $this->doIt(self::CODE[$mask], $value);
+    /**
+     * Add a cep mask
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function cep(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::CEP,
+                        $value
+                    );
+    }
+
+    /**
+     * Add a cpf mask
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function cpf(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::CPF,
+                        $value
+                    );
+    }
+
+    /**
+     * Add a cnpj mask
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function cnpj(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::CNPJ,
+                        $value
+                    );
+    }
+
+    /**
+     * Add a rg mask
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function rg(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::RG,
+                        $value
+                    );
+    }
+
+    /**
+     * Add a credit card mask hiding central characters
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function creditCard(string $value): string
+    {
+        return $this
+                    ->transform(
+                        Constants::CREDIT_CARD,
+                        $value
+                    );
+    }
+
+    /**
+     * Add a custom mask.
+     * Set the characters you want to replace with #
+     *
+     * @param string $value
+     * @param string $mask
+     *
+     * @return string
+     */
+    public function custom(
+        string $value,
+        string $mask
+    ): string {
+        $format = Constants::CUSTOM;
+        return $this->transform($format, $value, $mask);
+    }
+
+    /**
+     * Remove special characters
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public function clear(string $value): string
+    {
+        
+        $value = preg_replace(
+            '/[\@\.\;\-\?\/\|\*\_\~\!\$\&\(\)\{\}\:\=\+\^\´\`\"\'\," "]+/',
+            '',
+            $value
+        );
+
+        $value = str_replace('[','', $value);
+        $value = str_replace(']','', $value);
         
         return $value;
     }
 
-    public function doIt(
-        string $mask,
-        string $value
+    /**
+     * Transfom value with the mask
+     *
+     * @param string $format
+     * @param $value
+     * @param $mask = null
+     *
+     * @return string
+     */
+    private function transform(
+        string $format,
+        $value,
+        $mask = null
+    ): string {
+        $this->validate($value);
+        $mask = $this->getMask($format, $value, $mask);
+        $value = $this->clear($value);
+        $value = $this->autocomplete($format, $value);
+        $value = $this->do($value, $mask);
+        return $value;
+    }
+
+    /**
+     * Execute replace process
+     *
+     * @param string $value
+     * @param string $mask
+     *
+     * @return string
+     */
+    private function do(
+        string $value,
+        string $mask
     ): string {
         $masked = '';
-
         $k = 0;
-            
         for ($i = 0; $i <= strlen($mask)-1; $i++) {
             if ($mask[$i] == '#') {
                 if (isset($value[$k])) {
@@ -52,22 +191,108 @@ class Mask
                 }
             }
         }
-
         return $masked;
     }
 
-    public function rewrite(
-        string $mask,
+    /**
+     * Auto complete validate
+     *
+     * @param string $format
+     * @param string $value
+     *
+     * @return string
+     */
+    private function autocomplete(
+        string $format,
         string $value
-    ):string {
-        return str_pad($value, Constants::LENGTH[self::FORMAT[$mask]], "0", STR_PAD_LEFT);
+    ): string {
+        if (in_array($format, Constants::AUTO_COMPLETE)) {
+            $length = Constants::LENGTH[$format];
+            $value = $this->addZeros($value, $length);
+        }
+        return $value;
     }
 
-    public function clear(
-        string $unmask
-    ):string {
-        $unmasked = preg_replace('/[\@\.\;\-\," "]+/', '', $unmask);
-        
-        return $unmasked;
+    /**
+     * Add zero to the left
+     *
+     * @param string $value
+     * @param int $length
+     *
+     * @return string
+     */
+    private function addZeros(
+        string $value,
+        int $length
+    ): string {
+        return str_pad(
+            $value,
+            $length,
+            "0",
+            STR_PAD_LEFT
+        );
+    }
+
+    /**
+     * Validate value
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    private function validate($value): bool
+    {
+        if (is_null($value)) {
+            $this->error(Constants::ERROR['NULL']);
+        }
+        if (is_string($value)) {
+            return true;
+        }
+        if (is_int($value)) {
+            return true;
+        }
+        if (is_double($value)) {
+            return true;
+        }
+        $this->error(Constants::ERROR['INVALID']);
+    }
+
+    /**
+     * Get the mask format
+     *
+     * @param string $format
+     * @param string $value
+     * @param ?string $mask
+     *
+     * @return string
+     */
+    private function getMask(
+        string $format,
+        string $value,
+        ?string $mask
+    ): string {
+        if (!is_null($mask)) {
+            return $mask;
+        }
+
+        if ($format == Constants::PHONE) {
+            if (strlen($value) > Constants::LENGTH[Constants::PHONE]) {
+                $format = Constants::CELL_PHONE;
+            }
+        }
+        $mask = Constants::MASK[$format];
+        return $mask;
+    }
+
+    /**
+     * Error method
+     *
+     * @param string $message
+     */
+    private function error(string $message)
+    {
+        throw new \Exception(
+            'Goldbach Algorithms: '.$message
+        );
     }
 }
